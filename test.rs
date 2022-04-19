11e45199c554bc6086fa5a9f30fced28f540ad18
axtlos <axtlos@tar.black>
2022-04-19 14:06:49 +0200

    remove test.rs

--- test.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // generate task scheduler
-    let mut sched = TaskScheduler::new();
-    // create a task
-    let task = sched.create_task(|| {
-        println!("Hello, world!");
-    });
-}
