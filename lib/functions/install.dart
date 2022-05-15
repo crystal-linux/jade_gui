d30dbba4ca1e7fe771ee49c081de07670ab8bdec
axtlos <axtlos@tar.black>
2022-05-15 12:37:12 +0200

    actually run jade

--- lib/functions/install.dart
+++ lib/functions/install.dart
@@ -10,9 +10,8 @@ test(setOutput, running, setRunning, config, writeToLog) async {
     const filename = "/tmp/jade.json";
     await File(filename).writeAsString(config);
     writeToLog("Json config: $config");
-    var process =
-        //await Process.start('pkexec', ['/opt/jade_gui/scripts/jadewrapper.sh']);
-        await Process.start('/opt/jade_gui/scripts/jadeemu.sh', []);
+    var process = await Process.start('pkexec', ['jade', 'config', filename]);
+    //await Process.start('/opt/jade_gui/scripts/jadeemu.sh', []);
     process.stdout.transform(utf8.decoder).forEach(setOutput);
     setRunning(true);
   }
