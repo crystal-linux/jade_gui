c5517b282507757679c789b159cd88c2cb9c8f05
axtlos <axtlos@tar.black>
2022-07-01 23:57:08 +0200

    add user screen page

--- src/functions/keyboard_screen.py
+++ src/functions/keyboard_screen.py
@@ -1,3 +1,23 @@
+# keyboard_Screen.py
+
+#
+# Copyright 2022 user
+
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from gi.repository import Gtk, Adw
 from gettext import gettext as _
 
@@ -40,6 +60,7 @@ class KeyboardScreen(Adw.Bin):
     def selected_variant(self, widget, row):
         if row is not None:
             print("variant selected")
+            self.carousel.scroll_to(self.next_page, True)
         else:
             print("row is none!! variant")
 
